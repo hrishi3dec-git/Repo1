37fa0524309a8fcf6f6603355634fa8cc28a8f11
hrishi <hrishi_3december@yahoo.co.in>
2021-11-09 20:58:15 +0530

    checkin GeneralScript1.sql

--- GeneralScript1.sql
+++ GeneralScript1.sql
@@ -6,3 +6,5 @@ from sys.indexes  as A
 crosss join sys.databases as B
 
 --Few new lines are added to see the git pull affect.
+-------------------------------------------------
+--This is a new line
