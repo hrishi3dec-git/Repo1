48526e52ea4302ac3a7f37d00a543de684dec284
hrishi <hrishi_3december@yahoo.co.in>
2021-11-09 20:36:41 +0530

    Added 1 new line to GeneralScript.sql

--- GeneralScript1.sql
+++ GeneralScript1.sql
@@ -1,5 +1,8 @@
 use PestPac
 go
 
-Select * from sys.indexes
-select * from sys.databases
+Select *
+from sys.indexes  as A
+crosss join sys.databases as B
+
+--Few new lines are added to see the git pull affect.
